1c85b43e640adf636c0f8f7f207955f1f377a983
Thomas McCarthy <120833756+T-M-McCarthy@users.noreply.github.com>
2025-02-27 20:01:01 -0500

    Restaurant_Project.sql

--- Restaurant_Project.sql
+++ Restaurant_Project.sql
@@ -12,12 +12,20 @@ UPDATE fast_food_sales
 SET transaction_type = 'Unknown'
 WHERE transaction_type = '';
 
+-- Update the date section so every date is in the same format. 
+UPDATE fast_food_sales
+SET date = DATE_FORMAT(
+    STR_TO_DATE(REPLACE(date, '/', '-'), '%m-%d-%Y'), '%Y-%m-%d');
 
--- 2. What are the items on the menu?
+-- 2. Shows the earliest date of sale to the last date of sale.
+SELECT MIN(date) AS earliest_date, MAX(date) AS latest_date
+FROM fast_food_sales;
+
+-- 3. What are the items on the menu?
 SELECT DISTINCT item_name FROM fast_food_sales
 ORDER BY item_name;
 
--- 3. What are the least and most expensive item?
+-- 4. What are the least and most expensive item?
 
 -- Least Expensive
 SELECT DISTINCT item_name, item_type, MIN(item_price) as prices
@@ -31,34 +39,34 @@ FROM fast_food_sales
 GROUP BY item_name, item_type
 ORDER BY price DESC;
 
--- 4. What are the different item types available?
+-- 5. What are the different item types available?
 SELECT DISTINCT item_type FROM fast_food_sales;
 
--- 5. What is the quantity of items sold for each item type?
+-- 6. What is the quantity of items sold for each item type?
 SELECT item_type, SUM(quantity) as total_sold
 FROM fast_food_sales
 GROUP BY item_type
 ORDER BY item_type; 
 
--- 6. What is the total revenue of each item type?
+-- 7. What is the total revenue of each item type?
 SELECT item_type, SUM(transaction_amount) as total_sales
 FROM fast_food_sales
 GROUP BY item_type
 ORDER BY item_type; 
 
--- 7. How many sales occured for each time of day?
+-- 8. How many sales occured for each time of day?
 SELECT time_of_sale, count(order_id) as total_sales
 FROM fast_food_sales
 GROUP BY time_of_sale
 ORDER BY total_sales;
 
--- 8. How many transactions were there for each transaction type? 
+-- 9. How many transactions were there for each transaction type? 
 SELECT transaction_type, COUNT(order_id) AS total_transaction_type
 FROM fast_food_sales
 GROUP BY transaction_type
 ORDER BY total_transaction_type;
 
--- 9. What is the total revenue of all items?
+-- 10. What is the total revenue of all items?
 SELECT COALESCE(item_type, 'Total') as item_type, SUM(transaction_amount) as Revenue
 FROM fast_food_sales
 GROUP BY item_type WITH ROLLUP;
